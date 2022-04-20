cd564b40c2463a5be2d91172cdd8f6253b6d9dcf
Saurin Dashadia <me@saur.in>
2022-04-20 18:38:29 +0530

    Code optimisation, Language functions used.

--- picsascii.php
+++ picsascii.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  *
  * @link              http://symaticsolutions.com
@@ -10,7 +9,7 @@
  * Plugin Name:       PicsAscii
  * Plugin URI:        https://github.com/SymaticSolutions/PicsAscii
  * Description:       This plugin converts images to their ASCII representation. Useful for quick conversion for ASCII profile images.
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            Symatic Solutions
  * Author URI:        http://symaticsolutions.com
  * Auther Email:	  info@symaticsolutions.com
@@ -40,46 +39,58 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 /**
+ * Currently plugin version.
+ * Start at version 0.1.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define( 'PICSASCII_VERSION', '1.0.2' );
+
+/**
+ * A plugin name
+ */
+define( 'PICSASCII_NAME', 'picsascii' );
+
+/**
+ * Plugin directory path
+ */
+define( 'PICSASCII_DIR_PATH', plugin_dir_path( __FILE__ ) );
+
+/**
  * The code that runs during plugin activation.
  * This action is documented in includes/class-picsascii-activator.php
  */
 function activate_picsascii() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-picsascii-activator.php';
+	require_once PICSASCII_DIR_PATH . 'includes/class-picsascii-activator.php';
 	Picsascii_Activator::activate();
 }
+register_activation_hook( __FILE__, 'activate_picsascii' );
 
 /**
  * The code that runs during plugin deactivation.
  * This action is documented in includes/class-picsascii-deactivator.php
  */
 function deactivate_picsascii() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-picsascii-deactivator.php';
+	require_once PICSASCII_DIR_PATH . 'includes/class-picsascii-deactivator.php';
 	Picsascii_Deactivator::deactivate();
 }
-
-register_activation_hook( __FILE__, 'activate_picsascii' );
 register_deactivation_hook( __FILE__, 'deactivate_picsascii' );
 
 /**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
+ * The core plugin class that is used to define internationalization, admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-picsascii.php';
+require PICSASCII_DIR_PATH . 'includes/class-picsascii.php';
 
 /**
  * Begins execution of the plugin.
  *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
+ * Since everything within the plugin is registered via hooks, then kicking off the plugin from this point in the file
+ * does not affect the page life cycle.
  *
  * @since    1.0.0
  */
 function run_picsascii() {
-
 	$plugin = new Picsascii();
 	$plugin->run();
-
 }
 
 run_picsascii();
--- public/index.php
+++ public/index.php
@@ -1,8 +1,1 @@
 <?php // Silence is golden
-/**
- * By: Symatic Solutions <info@symaticsolutions.com>
- * Date: 21/1/17
- * Time: 2:43 PM
- * Website: http://www.symaticsolutions.com
- * Skype: symatic.solutions
- */
