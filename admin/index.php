9678068f4a9c86c6e8460a1695142127ea09d40f
Saurin Dashadia <me@saur.in>
2022-04-20 17:30:17 +0530

    - Updated admin layout. - Language functions used for static strings for multi-lingual support. - Code optimisation.

--- admin/index.php
+++ admin/index.php
@@ -1,12 +1,1 @@
 <?php // Silence is golden
-/**
- * Plugin By: Symatic Solutions <info@symaticsolutions.com>
- * Date: 21/1/17
- * Time: 2:43 PM
- * Website: http://www.symaticsolutions.com
- * Skype: symatic.solutions
- *
- * Code By: Jason Page <pagetelegram@gmail.com>
- * Date: 20/1/17
- * Website: http://bondfires.cc
- */
